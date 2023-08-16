b8d8890212ef50ec66d6167a18e3c35de8533d32
Gautam Dayal <gautam.dayal@yahoo.com>
2023-08-16 08:34:58 +0530

    Polished, courtesy of chatgpt

--- main.py
+++ main.py
@@ -9,47 +9,41 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+def fit_image_into_square(input_image):
+    dominant_color = input_image.resize((1, 1)).getpixel((0, 0))
+    width, height = input_image.size
+    target_size = max(width, height)
+
+    if width != height:
+        result_image = Image.new("RGB", (target_size, target_size), dominant_color)
+        if width > height:
+            target_pixel = (0, (width - height) // 2)
+        else:
+            target_pixel = ((height - width) // 2, 0)
+        result_image.paste(input_image, target_pixel)
+    else:
+        result_image = input_image.copy()  # No need to modify if already square
+
+    return result_image
+
 @app.route("/generate", methods=["POST"])
 def doImage():
-    # input_image = Image.open(filename).convert("RGB")
-    # file = request.files["png"]
-    # file.save("test_image.png")
-    input_image = Image.open(request.files["png"])
-    dominant_color = input_image.resize((1, 1)).getpixel((0, 0))
-    width = input_image.size[0]
-    height = input_image.size[1]
-    target_size = None
-    wbigger = False
-    print("After init")
-    if width == height:
-        return
-    if width > height:
-        target_size = width
-        wbigger = True
-    else:
-        target_size = height 
-    
-    result_image_color = Image.new("RGB", (target_size, target_size), dominant_color)
-    # result_image_white = Image.new("RGB", (target_size, target_size), (255, 255, 255))
-    # result_image_black = Image.new("RGB", (target_size, target_size), 0)
-    print("After setting blank canvases")
-    
-    target_pixel = None
-    if wbigger:
-        target_pixel = (0, (width - height) // 2)
-    else:
-        target_pixel = ((height - width) // 2, 0)
-    
-    result_image_color.paste(input_image, target_pixel)
-    # result_image_white.paste(input_image, target_pixel)
-    # result_image_black.paste(input_image, target_pixel)
-    print("After paste")
+    try:
+        uploaded_file = request.files["png"]
+        if uploaded_file.filename == "":
+            return "No file selected", 400
 
-    result_image_color.save(f"output/eeeee_color.png", type="PNG")
-    return send_file("output/eeeee_color.png")
-    # result_image_black.save(f"output/{filename}_black.png", type="PNG")
-    # result_image_white.save(f"output/{filename}_white.png", type="PNG")
-    print("After saving files")
+        image = Image.open(uploaded_file)
+        image = image.convert("RGB")
+
+        result_image = fit_image_into_square(image)
+
+        output_path = "output/eeeee_color.png"
+        result_image.save(output_path, format="PNG")
+
+        return send_file(output_path)
+    except Exception as e:
+        return f"An error occurred: {str(e)}", 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
